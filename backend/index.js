61e26d76afc52d5f2aa6af68f389dd5e1885657f
Moin Heranja <moin@example.com>
2026-07-07 12:37:49 +0530

    Updated CORS for Vercel

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://netflix-clone-hrav.onrender.com',
+    origin:'https://netflix-clone-frontend-ecru.vercel.app',
     credentials:true
 }
 app.use(cors(corsOptions));
